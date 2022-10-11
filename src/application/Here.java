5221c59f1dca466312901129d40c225447f5b031
Evanstanislas <115131773+Evanstanislas@users.noreply.github.com>
2022-10-11 18:19:48 +0700

    Shaping up
    Hoping that no more fix is required

--- src/application/Here.java
+++ src/application/Here.java
@@ -1,6 +1,7 @@
 package application;
 
-import javafx.application.Application; 
+import javafx.application.Application;
+import java.util.Random; 
 import javafx.scene.Group; 
 import javafx.scene.Scene; 
 import javafx.scene.paint.Color; 
@@ -17,19 +18,23 @@ import javafx.scene.layout.GridPane;
 
 
 public class Here extends Application {
+	//Fungsi buat RNG
+	public double getRandomNumber(Integer min, Integer max) {
+	    return (int) (Math.round((Math.random() * (max - min)) + min));
+	}
 
 	@Override  
     public void start(Stage primaryStage) throws Exception {  
         // TODO Auto-generated method stub
 		
-		//Initiate Integer
-		Integer MINIMAL;
-		Integer MAXIMAL;
-		
 		//Set Text for Heading
 		Text text= new Text();
 		text.setFont(new Font(24));
-		text.setText("You're trapped. Input 2 numbers to decides your door!");
+		text.setText("You're trapped! Input 2 numbers to decides your door!");
+		
+		//Next job, WIP
+//		Text res= new Text();
+//		text.setText("Looks like Door number " + Num + " is safe");
 		
 		//Set Label and Text for input
 		Label Min = new Label("Min door number");
@@ -40,14 +45,18 @@ public class Here extends Application {
 		
 		
 		//Set Button
-		Button Yenson= new Button("Hi My Friend");
-		Yenson.setScaleX(1.4);
-		Yenson.setScaleY(1.5);
+		Button Yenson= new Button("Help me!!!");
+		Yenson.setScaleX(1.6);
+		Yenson.setScaleY(1.7);
 		Yenson.setOnAction(new EventHandler<ActionEvent>(){
 			//jika diclick akan berjalan
-			public void handle(ActionEvent arg0) {  
+			public void handle(ActionEvent arg0) {
+				Integer MINIMAL =Integer.parseInt(a1.getText());
+				Integer MAXIMAL =Integer.parseInt(a2.getText());
+				
+				String Num= Double.toString(getRandomNumber(MINIMAL, MAXIMAL));
                 // TODO Auto-generated method stub  
-                System.out.println("Looks like Door number " +a1.getText()+ " is safe");
+                System.out.println("Looks like Door number " + Num + " is safe");
             }
 		});
 		
@@ -59,6 +68,7 @@ public class Here extends Application {
 		grid.addRow(2, Min, a1);
 		grid.addRow(3, Max, a2);
 		grid.addRow(4, Yenson);
+//		grid.addRow(5, res);
 		
 		//Scene
 		Scene scene= new Scene(grid, 700,500);
